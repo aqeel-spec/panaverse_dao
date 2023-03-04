11cd676e22c587ad930b5965b70f50cd590ec8d2
Aqeel Shahzad <67902032+aqeel-spec@users.noreply.github.com>
2023-03-04 20:51:09 +0500

    fix deployment error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b35df3e0f29feeaf"; }
+/******/ 		__webpack_require__.h = function() { return "7cbb117703805673"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
